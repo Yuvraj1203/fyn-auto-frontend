ce9a28688ae9aaff9ce27ef29d819c5f5c7d1a95
Yuvraj <yuvrajrathore1203@gmail.com>
2025-07-09 14:42:31 +0530

    Update Dashboard.tsx

--- src/components/pages/dashboard/Dashboard.tsx
+++ src/components/pages/dashboard/Dashboard.tsx
@@ -143,7 +143,6 @@ function Dashboard() {
                 onPress={() => {
                   router.push("/tenantInfo");
                 }}
-                // isLoading={loading}
               >
                 {"Start Tenant Creation"}
               </Button>
